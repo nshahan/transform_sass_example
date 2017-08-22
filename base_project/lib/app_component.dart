80170af513583f32ea8ce8fbe5952b5e6b8af848
Nicholas Shahan <nshahan@google.com>
2017-08-22 15:20:05 -0700

    Add simple repro

---
+++ base_project/lib/app_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+)
+class AppComponent {
+  // Nothing here yet.
+}
---
+++ your_project/web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular/angular.dart';
+import 'package:your_project/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
